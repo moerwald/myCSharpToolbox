1d20ef769b8e39ba625f64bb9f19dc9683124962
andreas mewald <andreas.mewald@pdts.at>
2017-12-18 08:58:08 +0100

    Added priorityQ

---
+++ source/CollectionTest/PriorityQueueTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PriorityQueues;
+
+namespace CollectionTest
+{
+    [TestClass]
+    public class PriorityQueueTest
+    {
+        public class Employee : IComparable<Employee>
+        {
+            public string lastName;
+            public double priority; // smaller values are higher priority
+
+            public Employee(string lastName, double priority)
+            {
+                this.lastName = lastName;
+                this.priority = priority;
+            }
+
+            public override string ToString()
+            {
+                return "(" + lastName + ", " + priority.ToString("F1") + ")";
+            }
+
+            public int CompareTo(Employee other)
+            {
+                if (this.priority < other.priority) return -1;
+                else if (this.priority > other.priority) return 1;
+                else return 0;
+            }
+        } // Employee
+
+
+        static void TestPriorityQueue(int numOperations)
+        {
+            Random rand = new Random(0);
+            PriorityQueue<Employee> pq = new PriorityQueue<Employee>();
+            for (int op = 0; op < numOperations; ++op)
+            {
+                int opType = rand.Next(0, 2);
+
+                if (opType == 0) // enqueue
+                {
+                    string lastName = op + "man";
+                    double priority = (100.0 - 1.0) * rand.NextDouble() + 1.0;
+                    pq.Enqueue(new Employee(lastName, priority));
+                    Assert.IsTrue(pq.IsConsistent(), $"Test fails after enqueue operation # {op}");
+                }
+                else // dequeue
+                {
+                    if (pq.Count() > 0)
+                    {
+                        Employee e = pq.Dequeue();
+                        Assert.IsTrue(pq.IsConsistent(), $"Test fails after enqueue operation # {op}");
+                    }
+                }
+            } // for
+        } // TestPriorityQueue
+
+
+        [TestMethod]
+        public void TestEnqueueAndDequeue()
+        {
+            #region Arrange
+
+            #endregion
+
+            #region Act
+            TestPriorityQueue(10);
+            #endregion
+
+            #region Assert
+
+            #endregion
+        }
+    }
+}
---
+++ source/Collections/PriorityQueue.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Demonstrate a Priority Queue implemented with a Binary Heap
+namespace PriorityQueues
+{
+    public class PriorityQueue<T> where T : IComparable<T>
+    {
+        // Implementation of the min binary heap
+        // Origin code from: https://visualstudiomagazine.com/Articles/2012/11/01/Priority-Queues-with-C.aspx?Page=2
+        // For interactive explanation see: https://www.cs.usfca.edu/~galles/visualization/Heap.html
+        private List<T> data;
+
+        private object lockObject = new object();
+
+        public PriorityQueue()
+        {
+            this.data = new List<T>();
+        }
+
+        public void Enqueue(T item)
+        {
+            lock (this.lockObject)
+            {
+                // When using a list as internal storage, the trick is that for any parent node in the list at index [pi], the two child nodes are located at indexes [2 * pi + 1] and [2 * pi + 2]. 
+                // For a given child node at index [ci], its parent is located at index [(ci - 1) / 2].
+                data.Add(item);
+                int ci = data.Count - 1; // child index; start at end
+                while (ci > 0)
+                {
+                    int pi = (ci - 1) / 2; // parent index
+                    if (data[ci].CompareTo(data[pi]) >= 0) break; // child item is larger than (or equal) parent so we're done
+
+                    T tmp = data[ci]; data[ci] = data[pi]; data[pi] = tmp;
+                    ci = pi;
+                }
+            }
+        }
+
+        public T Dequeue()
+        {
+            lock (this.lockObject)
+            {
+                // assumes pq is not empty; up to calling code
+                int lastIndex = data.Count - 1; // last index (before removal)
+                T frontItem = data[0];   // fetch the front
+                data[0] = data[lastIndex];
+                data.RemoveAt(lastIndex);
+
+                --lastIndex; // last index (after removal)
+                int parentIndex = 0; // parent index. start at front of pq
+                while (true)
+                {
+                    int leftChildIndex = parentIndex * 2 + 1; // left child index of parent
+                    if (leftChildIndex > lastIndex) break;  // no children so done
+                    int rightChildIndex = leftChildIndex + 1;     // right child
+                    if (rightChildIndex <= lastIndex && data[rightChildIndex].CompareTo(data[leftChildIndex]) < 0) // if there is a rc (ci + 1), and it is smaller than left child, use the rc instead
+                        leftChildIndex = rightChildIndex;
+                    if (data[parentIndex].CompareTo(data[leftChildIndex]) <= 0) break; // parent is smaller than (or equal to) smallest child so done
+                    T tmp = data[parentIndex]; data[parentIndex] = data[leftChildIndex]; data[leftChildIndex] = tmp; // swap parent and child
+                    parentIndex = leftChildIndex;
+                }
+                return frontItem;
+            }
+        }
+
+        public T Peek()
+        {
+            lock (this.lockObject)
+            {
+                T frontItem = data[0];
+                return frontItem;
+            }
+        }
+
+        public int Count()
+        {
+            return data.Count;
+        }
+
+        public override string ToString()
+        {
+            lock (this.lockObject)
+            {
+                string s = "";
+                for (int i = 0; i < data.Count; ++i)
+                    s += data[i].ToString() + " ";
+                s += "count = " + data.Count;
+                return s;
+            }
+        }
+
+        public bool IsConsistent()
+        {
+            lock (this.lockObject)
+            {
+                // is the heap property true for all data?
+                if (data.Count == 0) return true;
+                int lastIndex = data.Count - 1; // last index
+                for (int parentIndex = 0; parentIndex < data.Count; ++parentIndex) // each parent index
+                {
+                    int leftChildIndex = 2 * parentIndex + 1; // left child index
+                    int rightChildIndex = 2 * parentIndex + 2; // right child index
+
+                    if (leftChildIndex <= lastIndex && data[parentIndex].CompareTo(data[leftChildIndex]) > 0) return false; // if lc exists and it's greater than parent then bad.
+                    if (rightChildIndex <= lastIndex && data[parentIndex].CompareTo(data[rightChildIndex]) > 0) return false; // check the right child too.
+                }
+                return true; // passed all checks
+            }
+        } // IsConsistent
+    } // PriorityQueue
+
+} // ns
