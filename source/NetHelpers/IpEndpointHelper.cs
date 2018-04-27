68739798a316351575acbaa2d23674aa9f2e9f57
Mewald Andreas <MewaldA@pdts.at>
2018-04-27 07:26:23 +0200

    Add new project

---
+++ source/NetHelpers/IpEndpointHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+using System.Net;
+
+namespace NetHelpers
+{
+    public static class IPEndpointHelper
+    {
+        /// <summary>
+        /// Create IPEndpoint object from an IPv4 or IPv6 endpoint.
+        /// Note: Original code was taken from https://stackoverflow.com/questions/2727609/best-way-to-create-ipendpoint-from-string
+        /// </summary>
+        /// <param name="endPoint"></param>
+        /// <returns></returns>
+        public static IPEndPoint CreateIPEndPoint(string endPoint)
+        {
+            string[] ep = endPoint.Split(':');
+            if (ep.Length < 2)
+            {
+                throw new FormatException("Invalid endpoint format");
+            }
+
+            IPAddress ip;
+            if (ep.Length > 2)
+            {
+                // IPv4 address
+                if (!IPAddress.TryParse(string.Join(":", ep, 0, ep.Length - 1), out ip))
+                {
+                    throw new FormatException("Invalid ip-adress");
+                }
+            }
+            else
+            {
+                // IPv6 address
+                if (!IPAddress.TryParse(ep[0], out ip))
+                {
+                    throw new FormatException("Invalid ip-adress");
+                }
+            }
+
+            // Port parsing
+            if (!int.TryParse(ep[ep.Length - 1], NumberStyles.None, NumberFormatInfo.CurrentInfo, out var port))
+            {
+                throw new FormatException("Invalid port");
+            }
+            return new IPEndPoint(ip, port);
+        }
+    }
+}
