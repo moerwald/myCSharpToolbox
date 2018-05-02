6a057b96e21897dd0a8fea21ac469f197265bb19
Mewald Andreas <andreas.mewald@gmail.com>
2018-05-02 08:56:35 +0200

    Added Refernces project

---
+++ source/References/WriteOnce.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace References
+{
+    public class WriteOnce<T>
+    {
+        private readonly TaskCompletionSource<T> tcs = new TaskCompletionSource<T>();
+
+        public Task<T> ValueAsync => this.tcs.Task;
+        public T Value => this.tcs.Task.Result;
+
+        public bool TrySetInitialValue(T value)
+        {
+            try
+            {
+                this.tcs.SetResult(value);
+                return true;
+            }
+            catch (InvalidOperationException)
+            {
+                return false;
+            }
+        }
+
+        public void SetInitialValue(T value)
+        {
+            if (!TrySetInitialValue(value))
+                throw new InvalidOperationException("The value has already been set.");
+        }
+
+        public static implicit operator T(WriteOnce<T> readOnly) => readOnly.Value;
+        public static implicit operator Task<T>(WriteOnce<T> readOnly) => readOnly.ValueAsync;
+    }
+}
